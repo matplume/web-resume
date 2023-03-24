e0cc24c95705cddb4b94f61a2207d7058aedf239
MatPlume <matthieuwpetit@gmail.com>
2023-03-24 15:17:10 +0100

    Favicon updated

--- src/Components/Media/Media.js
+++ src/Components/Media/Media.js
@@ -1,29 +1,39 @@
-import React, { Component} from 'react';
-import{ Container } from 'reactstrap';
-import './media.css';
-import Typing from 'react-typing-animation';
+import React, { Component } from "react";
+import { Container } from "reactstrap";
+import "./media.css";
+import Typing from "react-typing-animation";
 
 class Media extends Component {
-    render () {
-        return (
-          <div>
-              <Container fluid={true} className="media-container">
-                <img src ={process.env.PUBLIC_URL + './images/media2.jpeg'} alt="mediaPicture" className="media-home"></img>
-                <div className="my-text">
-                  <Typing loop={true} speed={50}>
-                    <p><span>Matthieu Petit.</span></p>
-                    <Typing.Delay ms={1000} />
-                    <Typing.Backspace count={20} />
-                    <p><span>Développeur web.</span></p>
-                    <Typing.Delay ms={1000} />
-                    <Typing.Backspace count={20} />
-                    <p><span>Javascript. React. Node. Express.</span></p>
-                    <Typing.Reset count={2} delay={2000} />
-                  </Typing>
-                </div>
-              </Container>
-          </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<Container fluid={true} className="media-container">
+					<img
+						src={process.env.PUBLIC_URL + "./images/media2.jpeg"}
+						alt="mediaPicture"
+						className="media-home"
+					/>
+					<div className="my-text">
+						<Typing loop={true} speed={40}>
+							<p>
+								<span>Matthieu Petit.</span>
+							</p>
+							<Typing.Delay ms={1000} />
+							<Typing.Backspace count={20} />
+							<p>
+								<span>Développeur Front-End.</span>
+							</p>
+							<Typing.Delay ms={1000} />
+							<Typing.Backspace count={30} />
+							<p>
+								<span>Javascript. React. Redux.</span>
+							</p>
+							<Typing.Reset count={2} delay={2000} />
+						</Typing>
+					</div>
+				</Container>
+			</div>
+		);
+	}
 }
 export default Media;
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,81 +1,72 @@
-import React, { Component } from 'react';
-import './skills.css';
-import ScrollableAnchor from 'react-scrollable-anchor';
-import TagCloud from 'react-tag-cloud';
-
+import React, { Component } from "react";
+import "./skills.css";
+import ScrollableAnchor from "react-scrollable-anchor";
+import TagCloud from "react-tag-cloud";
 
 const styles = {
-    large: {
-      fontSize: 60,
-      fontWeight: 'bold'
-    },
-    small: {
-      opacity: 0.7,
-      fontSize: 16
-    }
-  };
-  
-  class App extends Component {
-    componentDidMount() {
-      setInterval(() => {
-        this.forceUpdate();
-      }, 4000);
-    }
-    render() {
-      return (
-        <ScrollableAnchor id={'Skills'}>
-          <div>
-              
-                  <div className="container-fluid wrap-custom-skills">
-                  <h3>COMPÉTENCES</h3>
-        <div className='app-outer'>
-          <div className='app-inner'>
-           
-            <TagCloud 
-              className='tag-cloud'
-              style={{
-                fontFamily: 'Poppins',
-                //fontSize: () => Math.round(Math.random() * 50) + 16,
-                fontSize: 30,
-                padding: 5,
-              }}>
-              <div style={styles.large}>Javascript</div>
-              <div style={styles.large}>React</div>
-              <div style={{fontWeight: 200}}>Express</div>
-              <div style={styles.large}>Node</div>
-              <div style={{fontFamily: 'arial'}}>Git</div>
-              <div style={{fontSize: 30}}>Scrum</div>
-              <div>Html5</div>
-              <div style={{fontWeight: 200}}>CSS3</div>
-              <div style={{fontWeight: 300}}>MongoDB</div>
-              <div style={{fontWeight: 300}}>MySQL</div>
-              <div style={{fontWeight: 200}}>Firebase</div>
-              <div style={{fontWeight: 200}}>Agiles</div>
-              <div style={{fontWeight: 200}}>Api</div>
-              <div style={{fontWeight: 200}}>Less</div>
-              <div className="javascript-skill" style={{fontWeight: 300}}>Javascript</div>
-              <div>Bootstrap</div>
-              <div className="tag-item-wrapper">
-              </div>
-              <div>Redux</div>
-              <div style={styles.small}>Npm</div>
-              <div style={styles.small}>Photoshop</div>
-              <div style={styles.small}>Final cut</div>
-              <div style={styles.small}>Sketch</div>
-              <div style={styles.small}>Ecmascript6</div>
-              <div style={styles.small}>Phaser</div>
-              <div style={styles.small}>Travis</div>
-            </TagCloud>
-          </div>
-        </div>
-        </div>
-      
-     
-        </div>
-        </ScrollableAnchor>
-      );
-    }
-  }
-  
-  export default App;
-  
+	large: {
+		fontSize: 60,
+		fontWeight: "bold",
+	},
+	small: {
+		opacity: 0.7,
+		fontSize: 16,
+	},
+};
+
+class App extends Component {
+	componentDidMount() {
+		setInterval(() => {
+			this.forceUpdate();
+		}, 4000);
+	}
+	render() {
+		return (
+			<ScrollableAnchor id={"Skills"}>
+				<div>
+					<div className="container-fluid wrap-custom-skills">
+						<h3>COMPÉTENCES</h3>
+						<div className="app-outer">
+							<div className="app-inner">
+								<TagCloud
+									className="tag-cloud"
+									style={{
+										fontFamily: "Poppins",
+										//fontSize: () => Math.round(Math.random() * 50) + 16,
+										fontSize: 30,
+										padding: 5,
+									}}
+								>
+									<div style={styles.large}>Javascript</div>
+									<div style={styles.large}>React</div>
+									<div style={styles.large}>Figma</div>
+									<div style={{ fontFamily: "arial" }}>Git</div>
+									<div style={{ fontSize: 30 }}>Scrum</div>
+									<div>Html5</div>
+									<div style={styles.large}>CSS3</div>
+									<div style={{ fontWeight: 300 }}>MySQL</div>
+									<div style={{ fontWeight: 200 }}>Agiles</div>
+									<div style={{ fontWeight: 200 }}>Liquid</div>
+									<div style={{ fontWeight: 200 }}>Less</div>
+									<div className="javascript-skill" style={{ fontWeight: 300 }}>
+										Javascript
+									</div>
+									<div>Bootstrap</div>
+									<div className="tag-item-wrapper" />
+									<div>Redux</div>
+									<div style={styles.small}>Npm</div>
+									<div style={styles.small}>Node</div>
+									<div style={styles.small}>Final cut</div>
+									<div style={styles.small}>Sketch</div>
+									<div style={styles.small}>Ant-Design</div>
+								</TagCloud>
+							</div>
+						</div>
+					</div>
+				</div>
+			</ScrollableAnchor>
+		);
+	}
+}
+
+export default App;
