2926cabade485df89f4fe72b93b91d698311768a
MatPlume <matthieuwpetit@gmail.com>
2023-03-24 16:18:37 +0100

    Updates

---
+++ precache-manifest.cb9efadf7bb7117f24b2beeb0e23e9b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a2292dfe00748f4278f",
+    "url": "/web-resume/static/js/runtime~main.3a2292df.js"
+  },
+  {
+    "revision": "7b735399bbb523f3a2fd",
+    "url": "/web-resume/static/js/main.7b735399.chunk.js"
+  },
+  {
+    "revision": "34ad3db17619a8440d45",
+    "url": "/web-resume/static/js/1.34ad3db1.chunk.js"
+  },
+  {
+    "revision": "7b735399bbb523f3a2fd",
+    "url": "/web-resume/static/css/main.2be76c16.chunk.css"
+  },
+  {
+    "revision": "34ad3db17619a8440d45",
+    "url": "/web-resume/static/css/1.37805fd0.chunk.css"
+  },
+  {
+    "revision": "ff94620074ae2c338623f9ab886bd7f4",
+    "url": "/web-resume/index.html"
+  }
+];
